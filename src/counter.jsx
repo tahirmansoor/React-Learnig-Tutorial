6ec888dd8d2bd12a46d7eea38466b541f570a1c2
tahirmansoor <86124089+tahirmansoor@users.noreply.github.com>
2025-06-11 12:59:39 +0500

    Using Use State

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,21 +1,23 @@
-function Counter(){
+import { useState } from "react";
 
+function Counter() {
+  let [counter, setCounter] = useState(15);
 
-let counter = 215;    
-const addValue = () =>{
-    console.log("click" , counter) 
- counter = counter + 1;
-}
+  const addValue = () => {
+    setCounter(counter + 1);
+  };
 
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
 
-    return(
-        <>
-           <p> Counter value is {counter}</p>
-           <button onClick={addValue}>Add  </button>
-           <button>Remove {counter}</button>
-            <p>sdsdsd {counter}</p>
-        </>
-
-    )
+  return (
+    <>
+      <p> Counter value is {counter}</p>
+      <button onClick={addValue}>Add </button>
+      <button onClick={removeValue}>Remove {counter}</button>
+      <p>sdsdsd {counter}</p>
+    </>
+  );
 }
 export default Counter;
